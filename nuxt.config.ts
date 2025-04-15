46093588919f7512bed206fd4b3f8a77262515f4
flehma <flehm97@icloud.com>
2025-04-15 13:41:52 +0200

    fcw-app.cloud allowedHosts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
-  devtools: { enabled: true },
+  devtools: { enabled: false },
   modules: ['@nuxtjs/tailwindcss'],
   nitro: {
     experimental: {
       database: true
     }
+  },
+  server: {
+    allowedHosts: ['localhost', 'fcw-app.cloud']
   }
 })
