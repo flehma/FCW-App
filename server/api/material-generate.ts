75fef7242c480a91485a322dc588738646a0dbdb
flehma <flehm97@icloud.com>
2025-04-24 18:04:34 +0200

    hauke cant be generated

--- server/api/material-generate.ts
+++ server/api/material-generate.ts
@@ -4,15 +4,11 @@ export default defineEventHandler(async () => {
     try {
         
         const connection = await getConnection();
-        const [flaschen] = await connection.execute(`SELECT k.id, k.name, m.flaschen FROM material m JOIN kader k ON k.id = m.spielerId WHERE m.spielerId IN (SELECT spielerId FROM training) AND m.spielerId IN (SELECT spielerId FROM spieltag) ORDER BY m.flaschen, k.name;`);
-        const [musikbox] = await connection.execute(`SELECT k.id, k.name, m.musikbox FROM material m JOIN kader k ON k.id = m.spielerId WHERE m.spielerId IN (SELECT spielerId FROM training) AND m.spielerId IN (SELECT spielerId FROM spieltag) AND k.id NOT IN (${flaschen[0].id}) ORDER BY m.musikbox, k.name;`);
+        const [flaschen] = await connection.execute(`SELECT k.id, k.name, m.flaschen FROM material m JOIN kader k ON k.id = m.spielerId WHERE m.spielerId IN (SELECT spielerId FROM training) AND m.spielerId IN (SELECT spielerId FROM spieltag) AND k.id NOT IN (6) ORDER BY m.flaschen, k.name;`);
+        const [musikbox] = await connection.execute(`SELECT k.id, k.name, m.musikbox FROM material m JOIN kader k ON k.id = m.spielerId WHERE m.spielerId IN (SELECT spielerId FROM training) AND m.spielerId IN (SELECT spielerId FROM spieltag) AND k.id NOT IN (${flaschen[0].id}, 6) ORDER BY m.musikbox, k.name;`);
         const [bälle] = await connection.execute(`SELECT k.id, k.name, m.bälle FROM material m JOIN kader k ON k.id = m.spielerId WHERE m.spielerId IN (SELECT spielerId FROM training) AND m.spielerId IN (SELECT spielerId FROM spieltag) AND k.id NOT IN (${flaschen[0].id}, ${musikbox[0].id}, 3, 6, 31) ORDER BY m.bälle, k.name;`);
-        const [jacken] = await connection.execute(`SELECT k.id, k.name, m.jacken FROM material m JOIN kader k ON k.id = m.spielerId WHERE m.spielerId IN (SELECT spielerId FROM training) AND m.spielerId IN (SELECT spielerId FROM spieltag)  AND k.id NOT IN (${flaschen[0].id}, ${musikbox[0].id}, ${bälle[0].id}) ORDER BY m.jacken, k.name;`);
+        const [jacken] = await connection.execute(`SELECT k.id, k.name, m.jacken FROM material m JOIN kader k ON k.id = m.spielerId WHERE m.spielerId IN (SELECT spielerId FROM training) AND m.spielerId IN (SELECT spielerId FROM spieltag)  AND k.id NOT IN (${flaschen[0].id}, ${musikbox[0].id}, ${bälle[0].id}, 6) ORDER BY m.jacken, k.name;`);
         await connection.end();
-        console.log('flaschen', flaschen)
-        console.log('musikbox', musikbox)
-        console.log('bälle', bälle)
-        console.log('jacken', jacken)
         return {
             flaschen: flaschen,
             musikbox: musikbox,
