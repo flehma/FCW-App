3a99c299a4ee45adab233c33cd52a3742d9bac2f
flehma <flehm97@icloud.com>
2025-04-24 18:07:00 +0200

    checkmark when already in training/kader

--- server/api/kader-show.ts
+++ server/api/kader-show.ts
@@ -3,7 +3,7 @@ import { getConnection } from '../utils/db.js';
 export default defineEventHandler(async () => {
     try {
         const connection = await getConnection();
-        const [rows] = await connection.execute('SELECT * FROM kader LEFT JOIN material ON kader.id = material.spielerId');
+        const [rows] = await connection.execute('SELECT k.*, m.flaschen, m.musikbox, m.`bälle`, m.jacken, s.spielerId AS `spieltag`, t.spielerId AS `training` FROM kader k LEFT JOIN material m ON k.id = m.spielerId LEFT JOIN spieltag s ON s.spielerId = k.id LEFT JOIN training t ON t.spielerId = k.id ORDER BY k.name');
         await connection.end();
         return {
             kader: rows
