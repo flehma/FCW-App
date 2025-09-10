edd7da3eeda2ac00651791c95f6e66e5d194190a
flehma <flehm97@icloud.com>
2025-09-10 17:13:41 +0200

    debug

--- server/api/strafen-eintragen.ts
+++ server/api/strafen-eintragen.ts
@@ -14,6 +14,8 @@ export default defineEventHandler(async (event: any) => {
         
         const [rows] = await connection.execute(`UPDATE spieler_strafen SET ${update} where spielerId = ${requestBody.spieler.id};`);
         console.log('UPDATE spieler_strafen SET ${update} where spielerId = ${requestBody.spieler.id};')
+        console.log('update: ' + update)
+        console.log('spielerId: ' + requestBody.spieler.id)
         await connection.end();
         return {
             kader: rows
