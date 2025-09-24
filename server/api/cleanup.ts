f295b6526153acba77458e9ae63d274d2b33bf4e
flehma <flehm97@icloud.com>
2025-09-24 15:42:13 +0200

    one time delete

--- server/api/cleanup.ts
+++ server/api/cleanup.ts
@@ -3,11 +3,15 @@ import { getConnection } from '../utils/db.js';
 export default defineEventHandler(async () => {
     try {
         const connection = await getConnection();
-        const [rows] = await connection.execute(`DELETE FROM kader where id IN (100, 101);`);
-        const [rows1] = await connection.execute(`DELETE FROM strafen where id IN (17);`);
+        await connection.execute(`DELETE FROM tunnel where id IN (100, 101);`);
+        await connection.execute(`DELETE FROM strafkisten_log where id IN (100, 101);`);
+        await connection.execute(`DELETE FROM spieler_strafen where id IN (100, 101);`);
+        await connection.execute(`DELETE FROM material where id IN (100, 101);`);
+        await connection.execute(`DELETE FROM kader where id IN (100, 101);`);
+        await connection.execute(`DELETE FROM strafen where id IN (17);`);
         await connection.end();
         return {
-            kader: rows
+            kader: "success"
         };
     } catch (error) {
         console.error('Database error:', error);
