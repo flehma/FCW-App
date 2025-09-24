e83aa3e0bad377e3c001454fcacd75a169a95729
flehma <flehm97@icloud.com>
2025-09-24 15:46:51 +0200

    clean cleanup

--- server/api/cleanup.ts
+++ server/api/cleanup.ts
@@ -3,7 +3,6 @@ import { getConnection } from '../utils/db.js';
 export default defineEventHandler(async () => {
     try {
         const connection = await getConnection();
-        await connection.execute(`DELETE FROM strafen where id IN (16);`);
         await connection.end();
         return {
             kader: "success"
