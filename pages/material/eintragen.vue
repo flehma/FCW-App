880ce3812107298d192ed6530f514ab0efc4a8e1
flehma <flehm97@icloud.com>
2025-10-05 15:34:33 +0200

    new material design

--- pages/material/eintragen.vue
+++ pages/material/eintragen.vue
@@ -1,115 +1,114 @@
 <template>
-    <div class="p-2">
-        <h2 class="text-lg font-bold mt-4">Flaschen</h2>
-        <div class="flex flex-row gap-2 mr-2">
-            <select class="dropdown p-2" v-model="selectedFlaschen">
-                <option v-for="spieler in kader?.kader" :value="spieler">
-                    {{ spieler.name }}
-                </option>
-            </select>
-            <button @click="addFlaschen" class="p-2 px-4 bg-blue-500 text-white rounded hover:bg-blue-400">
-                <svg width="30px" height="30px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M23.228 8.01785C23.6186 7.62741 23.6187 6.99424 23.2283 6.60363L22.5213 5.89638C22.1309 5.50577 21.4977 5.50563 21.1071 5.89607L10.0862 16.9122C9.69563 17.3027 9.6955 17.9359 10.0859 18.3265L10.7929 19.0337C11.1833 19.4243 11.8165 19.4245 12.2071 19.034L23.228 8.01785Z" fill="#FFFFFF"/><path d="M17.2285 8.01777C17.619 7.62724 17.619 6.99408 17.2285 6.60356L16.5214 5.89645C16.1309 5.50592 15.4977 5.50592 15.1072 5.89645L5.54542 15.4582L2.76773 12.6805C2.37721 12.29 1.74404 12.29 1.35352 12.6805L0.646409 13.3876C0.255884 13.7782 0.255885 14.4113 0.646409 14.8019L4.83831 18.9938C5.22883 19.3843 5.862 19.3843 6.25252 18.9938L17.2285 8.01777Z" fill="#FFFFFF"/>
-                </svg>
-            </button>
-        </div>
-        <h2 class="text-lg font-bold mt-4">Musikbox</h2>
-        <div class="flex flex-row gap-2 mr-2">
-            <select class="dropdown p-2" v-model="selectedMusikbox">
-                <option v-for="spieler in kader?.kader" :value="spieler">
-                    {{ spieler.name }}
-                </option>
-            </select>
-            <button @click="addMusikbox" class="p-2 px-4 bg-blue-500 text-white rounded hover:bg-blue-400">
-                <svg width="30px" height="30px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M23.228 8.01785C23.6186 7.62741 23.6187 6.99424 23.2283 6.60363L22.5213 5.89638C22.1309 5.50577 21.4977 5.50563 21.1071 5.89607L10.0862 16.9122C9.69563 17.3027 9.6955 17.9359 10.0859 18.3265L10.7929 19.0337C11.1833 19.4243 11.8165 19.4245 12.2071 19.034L23.228 8.01785Z" fill="#FFFFFF"/><path d="M17.2285 8.01777C17.619 7.62724 17.619 6.99408 17.2285 6.60356L16.5214 5.89645C16.1309 5.50592 15.4977 5.50592 15.1072 5.89645L5.54542 15.4582L2.76773 12.6805C2.37721 12.29 1.74404 12.29 1.35352 12.6805L0.646409 13.3876C0.255884 13.7782 0.255885 14.4113 0.646409 14.8019L4.83831 18.9938C5.22883 19.3843 5.862 19.3843 6.25252 18.9938L17.2285 8.01777Z" fill="#FFFFFF"/>
-                </svg>
-            </button>
-        </div>
-        <h2 class="text-lg font-bold mt-4">Bälle</h2>
-        <div class="flex flex-row gap-2 mr-2">
-            <select class="dropdown p-2" v-model="selectedBälle">
-                <option v-for="spieler in kader?.kader" :value="spieler">
-                    {{ spieler.name }}
-                </option>
-            </select>
-            <button @click="addBälle" class="p-2 px-4 bg-blue-500 text-white rounded hover:bg-blue-400">
-                <svg width="30px" height="30px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M23.228 8.01785C23.6186 7.62741 23.6187 6.99424 23.2283 6.60363L22.5213 5.89638C22.1309 5.50577 21.4977 5.50563 21.1071 5.89607L10.0862 16.9122C9.69563 17.3027 9.6955 17.9359 10.0859 18.3265L10.7929 19.0337C11.1833 19.4243 11.8165 19.4245 12.2071 19.034L23.228 8.01785Z" fill="#FFFFFF"/><path d="M17.2285 8.01777C17.619 7.62724 17.619 6.99408 17.2285 6.60356L16.5214 5.89645C16.1309 5.50592 15.4977 5.50592 15.1072 5.89645L5.54542 15.4582L2.76773 12.6805C2.37721 12.29 1.74404 12.29 1.35352 12.6805L0.646409 13.3876C0.255884 13.7782 0.255885 14.4113 0.646409 14.8019L4.83831 18.9938C5.22883 19.3843 5.862 19.3843 6.25252 18.9938L17.2285 8.01777Z" fill="#FFFFFF"/>
-                </svg>
-            </button>
-        </div>
-        <h2 class="text-lg font-bold mt-4">Jacken</h2>
-        <div class="flex flex-row gap-2 mr-2">
-            <select class="dropdown p-2" v-model="selectedJacken">
-                <option v-for="spieler in kader?.kader" :value="spieler">
-                    {{ spieler.name }}
-                </option>
-            </select>
-            <button @click="addJacken" class="p-2 px-4 bg-blue-500 text-white rounded hover:bg-blue-400">
-                <svg width="30px" height="30px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M23.228 8.01785C23.6186 7.62741 23.6187 6.99424 23.2283 6.60363L22.5213 5.89638C22.1309 5.50577 21.4977 5.50563 21.1071 5.89607L10.0862 16.9122C9.69563 17.3027 9.6955 17.9359 10.0859 18.3265L10.7929 19.0337C11.1833 19.4243 11.8165 19.4245 12.2071 19.034L23.228 8.01785Z" fill="#FFFFFF"/><path d="M17.2285 8.01777C17.619 7.62724 17.619 6.99408 17.2285 6.60356L16.5214 5.89645C16.1309 5.50592 15.4977 5.50592 15.1072 5.89645L5.54542 15.4582L2.76773 12.6805C2.37721 12.29 1.74404 12.29 1.35352 12.6805L0.646409 13.3876C0.255884 13.7782 0.255885 14.4113 0.646409 14.8019L4.83831 18.9938C5.22883 19.3843 5.862 19.3843 6.25252 18.9938L17.2285 8.01777Z" fill="#FFFFFF"/>
-                </svg>
-            </button>
-        </div>
+  <div class="p-4">
+    <h2 class="text-2xl font-bold mb-6 text-slate-700">Materialverwaltung</h2>
+
+    <!-- Materialgruppen dynamisch -->
+    <div v-for="item in materialItems" :key="item.label" class="mb-5">
+      <h3 class="text-lg font-semibold text-slate-700 mb-2">{{ item.label }}</h3>
+
+      <div class="flex gap-2">
+        <select
+          v-model="item.selected"
+          class="w-full px-3 py-2 border border-slate-300 rounded-lg focus:ring-2 focus:ring-blue-400 focus:outline-none transition"
+        >
+          <option disabled value="">-- Spieler auswählen --</option>
+          <option v-for="spieler in kader?.kader" :key="spieler.id" :value="spieler">
+            {{ spieler.name }}
+          </option>
+        </select>
+
+        <button
+          @click="item.action()"
+          class="flex items-center justify-center w-12 h-12 bg-blue-500 text-white rounded-lg hover:bg-blue-400 transition"
+        >
+          <!-- dein Haken-Icon -->
+          <svg
+            class="w-6 h-6"
+            viewBox="0 0 24 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M23.228 8.01785C23.6186 7.62741 23.6187 6.99424 23.2283 6.60363L22.5213 5.89638C22.1309 5.50577 21.4977 5.50563 21.1071 5.89607L10.0862 16.9122C9.69563 17.3027 9.6955 17.9359 10.0859 18.3265L10.7929 19.0337C11.1833 19.4243 11.8165 19.4245 12.2071 19.034L23.228 8.01785Z"
+              fill="#FFFFFF"
+            />
+            <path
+              d="M17.2285 8.01777C17.619 7.62724 17.619 6.99408 17.2285 6.60356L16.5214 5.89645C16.1309 5.50592 15.4977 5.50592 15.1072 5.89645L5.54542 15.4582L2.76773 12.6805C2.37721 12.29 1.74404 12.29 1.35352 12.6805L0.646409 13.3876C0.255884 13.7782 0.255885 14.4113 0.646409 14.8019L4.83831 18.9938C5.22883 19.3843 5.862 19.3843 6.25252 18.9938L17.2285 8.01777Z"
+              fill="#FFFFFF"
+            />
+          </svg>
+        </button>
+      </div>
     </div>
-    <div class="flex mt-4 flex-col items-center">
-        <RouterLink to="/material">
-            <button class="inline-flex items-center border border-blue-300 px-3 py-1.5 rounded-md text-blue-500 hover:bg-blue-50">Zurück</button>
-        </RouterLink>        
+
+    <!-- Zurück Button -->
+    <div class="flex justify-center mt-8">
+      <RouterLink to="/material">
+        <button
+          class="inline-flex items-center border border-blue-300 px-4 py-2 rounded-lg text-blue-600 font-medium hover:bg-blue-50 transition"
+        >
+          Zurück
+        </button>
+      </RouterLink>
     </div>
+  </div>
 </template>
+
 <script setup lang="ts">
+const kader = ref<any>([]);
+kader.value = await $fetch("/api/kader-show");
 
-    const kader = ref<any>([]);
-    kader.value = await $fetch('/api/kader-show')
+// einzelne Refs für Auswahl
+const selectedFlaschen = ref("");
+const selectedMusikbox = ref("");
+const selectedBälle = ref("");
+const selectedJacken = ref("");
 
-    const selectedFlaschen = ref('');
-    const selectedMusikbox = ref('');
-    const selectedBälle = ref('');
-    const selectedJacken = ref('');
-
-    async function addFlaschen() {
-        const res = await $fetch('/api/material-add-flaschen', {
-            method: "POST",
-            body: {
-                spieler: selectedFlaschen.value
-            }
-        }).then( async () => {
-            kader.value = await $fetch('/api/kader-show')
-        })
-    }
-
-    async function addMusikbox() {
-        const res = await $fetch('/api/material-add-musikbox', {
-            method: "POST",
-            body: {
-                spieler: selectedMusikbox.value
-            }
-        }).then( async () => {
-            kader.value = await $fetch('/api/kader-show')
-        })
-    }
-
-    async function addBälle() {
-        const res = await $fetch('/api/material-add-baelle', {
-            method: "POST",
-            body: {
-                spieler: selectedBälle.value
-            }
-        }).then( async () => {
-            kader.value = await $fetch('/api/kader-show')
-        })
-    }
-
-    async function addJacken() {
-        const res = await $fetch('/api/material-add-jacken', {
-            method: "POST",
-            body: {
-                spieler: selectedJacken.value
-            }
-        }).then( async () => {
-            kader.value = await $fetch('/api/kader-show')
-        })
-    }
+// dynamische Materialliste
+const materialItems = [
+  {
+    label: "Flaschen",
+    selected: selectedFlaschen,
+    action: async () => {
+      await $fetch("/api/material-add-flaschen", {
+        method: "POST",
+        body: { spieler: selectedFlaschen.value },
+      });
+      kader.value = await $fetch("/api/kader-show");
+    },
+  },
+  {
+    label: "Musikbox",
+    selected: selectedMusikbox,
+    action: async () => {
+      await $fetch("/api/material-add-musikbox", {
+        method: "POST",
+        body: { spieler: selectedMusikbox.value },
+      });
+      kader.value = await $fetch("/api/kader-show");
+    },
+  },
+  {
+    label: "Bälle",
+    selected: selectedBälle,
+    action: async () => {
+      await $fetch("/api/material-add-baelle", {
+        method: "POST",
+        body: { spieler: selectedBälle.value },
+      });
+      kader.value = await $fetch("/api/kader-show");
+    },
+  },
+  {
+    label: "Jacken",
+    selected: selectedJacken,
+    action: async () => {
+      await $fetch("/api/material-add-jacken", {
+        method: "POST",
+        body: { spieler: selectedJacken.value },
+      });
+      kader.value = await $fetch("/api/kader-show");
+    },
+  },
+];
 </script>
--- pages/material/verteilen.vue
+++ pages/material/verteilen.vue
@@ -1,27 +1,99 @@
 <template>
-    <div class="p-4" v-if="material.response == 'error'">Es müssen zuerst Spieler zum Training/Spieltag hinzugefügt werden</div>
-    <div class="p-4" v-if="material.response != 'error'">
-        *Materialausgabe Spieltag*<br><br>
-        *Flaschen* 🥤: @{{ material.flaschen[0].name }}<br><br>
-        *Bälle* ⚽: @{{ material.bälle[0].name }}<br><br>
-        *Musikbox* 🎵:  @{{ material.musikbox[0].name }}<br><br>
-        *Jacken* 🧥: @{{ material.jacken[0].name }} falls wir die brauchen<br><br>
-        *Kühlkoffer* 🚑: @Hauke Güntrath-Lorenzen<br><br>
-        Bitte heute nach dem Training mitnehmen!<br><br>
-        _Die Aufgabe zählt sowie für die Hin- und auch Abreise_<br><br>
-        Bitte kümmert euch selbstständig darum Ersatz zu finden, falls ihr keine Möglichkeit habt euer Material zu transportieren.
+  <div class="p-4">
+    <!-- Fehlerausgabe -->
+    <div
+      v-if="material.response === 'error'"
+      class="bg-red-50 text-red-700 p-4 rounded-lg border border-red-200"
+    >
+      Es müssen zuerst Spieler zum Training/Spieltag hinzugefügt werden.
     </div>
-    <div class="flex mt-4 flex-col items-center">
-        <RouterLink to="/material">
-            <button class="inline-flex items-center border border-blue-300 px-3 py-1.5 rounded-md text-blue-500 hover:bg-blue-50">Zurück</button>
-        </RouterLink>        
+
+    <!-- Materialausgabe -->
+    <div
+      v-else
+      class="relative bg-slate-50 border border-slate-200 rounded-xl shadow-sm p-5 text-slate-800"
+    >
+      <!-- Copy Button -->
+      <button
+        @click="copyToClipboard"
+        class="absolute top-3 right-3 flex items-center gap-1 text-sm text-blue-600 hover:text-blue-800 transition"
+      >
+        <svg
+          v-if="!copied"
+          xmlns="http://www.w3.org/2000/svg"
+          class="w-5 h-5"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+          stroke-width="2"
+        >
+          <rect x="9" y="9" width="13" height="13" rx="2" ry="2" />
+          <path d="M5 15H4a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2h9a2 2 0 0 1 2 2v1" />
+        </svg>
+        <svg
+          v-else
+          xmlns="http://www.w3.org/2000/svg"
+          class="w-5 h-5 text-green-600"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+          stroke-width="2"
+        >
+          <path stroke-linecap="round" stroke-linejoin="round" d="M5 13l4 4L19 7" />
+        </svg>
+        <span v-if="!copied">Kopieren</span>
+        <span v-else>✓ Kopiert!</span>
+      </button>
+
+      <!-- Textinhalt -->
+      <div ref="textRef" class="whitespace-pre-line leading-relaxed">
+        *Materialausgabe Spieltag*  
+        <br><br>
+        *Flaschen* 🥤: @{{ material.flaschen[0].name }}  
+        <br><br>
+        *Bälle* ⚽: @{{ material.bälle[0].name }}  
+        <br><br>
+        *Musikbox* 🎵: @{{ material.musikbox[0].name }}  
+        <br><br>
+        *Jacken* 🧥: @{{ material.jacken[0].name }} falls wir die brauchen  
+        <br><br>
+        *Kühlkoffer* 🚑: @Hauke Güntrath-Lorenzen  
+        <br><br>
+        Bitte heute nach dem Training mitnehmen!  
+        <br><br>
+        _Die Aufgabe zählt sowohl für die Hin- als auch die Abreise_  
+        <br><br>
+        Bitte kümmert euch selbstständig darum, Ersatz zu finden, falls ihr keine Möglichkeit habt, euer Material zu transportieren.
+      </div>
     </div>
-    
+
+    <!-- Zurück Button -->
+    <div class="flex mt-6 justify-center">
+      <RouterLink to="/material">
+        <button
+          class="inline-flex items-center border border-blue-300 px-4 py-2 rounded-lg text-blue-600 hover:bg-blue-50 transition"
+        >
+          Zurück
+        </button>
+      </RouterLink>
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts">
-    
 const material = ref<any>([]);
-    material.value = await $fetch('/api/material-generate')
+material.value = await $fetch("/api/material-generate");
 
+const textRef = ref<HTMLElement | null>(null);
+const copied = ref(false);
+
+function copyToClipboard() {
+  const text = textRef.value?.innerText || "";
+  navigator.clipboard.writeText(text);
+  copied.value = true;
+
+  setTimeout(() => {
+    copied.value = false;
+  }, 2000);
+}
 </script>
