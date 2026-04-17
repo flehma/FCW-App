87b9fb658c7460da18be6e6697b5216043742d01
flehma <flehm97@icloud.com>
2026-04-17 12:59:05 +0200

    clear cleanup and local docker compose

--- server/api/cleanup.ts
+++ server/api/cleanup.ts
@@ -3,7 +3,6 @@ import { getConnection } from '../utils/db.js';
 export default defineEventHandler(async () => {
     try {
         const connection = await getConnection();
-        const [rows] = await connection.execute(`UPDATE tunnel SET tunnel = 0 WHERE spielerId = 5;`);
         await connection.end();
         return {
             kader: "success"
